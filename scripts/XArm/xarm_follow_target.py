1b364ec7728d40f4be8b0175767478b342e568f9
Matthew Zane <mzane@ucsd.edu>
2023-05-18 13:06:11 +0900

    Add Face Searching Functionality

--- scripts/XArm/xarm_follow_target.py
+++ scripts/XArm/xarm_follow_target.py
@@ -18,7 +18,7 @@ class XArmFollowTarget(tasks.FollowTarget):
     def __init__(self):
         super().__init__(name="xarm_follow_target_task", target_position=np.array([0.3, 0.0, 0.5]) / get_stage_units(), offset=None)
         self._goal_position = np.array([0, 0, 0])
-        self._task_achieved = False
+        self.task_achieved = False
         return
     
     def set_up_scene(self, scene):
@@ -34,21 +34,11 @@ class XArmFollowTarget(tasks.FollowTarget):
         end_effector_position, _ = self._xarm.end_effector.get_world_pose()
         # print("orientation"+str(orient))
         dist = np.mean(np.abs(self._goal_position - end_effector_position))
-        if not self._task_achieved is bool(dist < 0.02):
-            self._task_achieved = bool(dist < 0.02)
-            if self._task_achieved:
+        if not self.task_achieved is bool(dist < 0.02):
+            self.task_achieved = bool(dist < 0.02)
+            if self.task_achieved:
                 print("Target Reached")
                 self._cube.get_applied_visual_material().set_color(color=np.array([0, 1.0, 0]))
-
-                # set new location
-                # randpos = [
-                #     np.random.uniform(0.3, 0.5), 
-                #     np.random.uniform(-0.3, 0.3),
-                #     np.random.uniform(0.3, 0.625)
-                #     ]
-                # print("Setting new target pos:"+str(randpos))
-                # self._cube.set_world_pose(np.array(randpos))
-                # self._cube.set_world_pose(np.array(randpos), np.array([180.0, 90, -180]))
             else:
                 self._cube.get_applied_visual_material().set_color(color=np.array([0, 0, 1.0]))
         return
--- scripts/XArm/xarm_sample.py
+++ scripts/XArm/xarm_sample.py
@@ -3,6 +3,8 @@ from .xarm_follow_target import XArmFollowTarget
 from .xarm_rmpflow_controller import XArmRMPFlowController
 from .xarm_socket import XArmSocket
 import numpy as np
+import time
+import carb
 
 class XArmSample(BaseSample):
     def __init__(self) -> None:
@@ -12,10 +14,15 @@ class XArmSample(BaseSample):
         # sending position data to arm
         self.xarm_socket = XArmSocket()
 
-        self._safe_zone = [
-            (0.3, -0.3, 0.3), # back bottom right 
-            (0.5, 0.3, 0.625) # top front left
-        ]
+        self._max_range = 0.7
+        self._min_range = 0.3
+        self._min_height = 0.1
+
+        self._last_face_seen_timeout = 1
+        self._last_face_seen_time = 0 
+
+        self._last_rand_target_timeout = 50
+        self._last_rand_target_time = 0 
 
     def setup_scene(self):
         world = self.get_world()
@@ -78,39 +85,55 @@ class XArmSample(BaseSample):
                 self.xarm_socket.txconn = None
 
         # update position of target?
+        current_time = time.time()
         if self.xarm_socket.dx and self.xarm_socket.dy:
             world = self.get_world()
             cube = world.scene.get_object("target")
             pos, _ = cube.get_world_pose()
-            newpose = [ pos[0], pos[1] + self.xarm_socket.dx, pos[2] + self.xarm_socket.dy]
-            newpose[1] = np.clip(newpose[1], self._safe_zone[0][1], self._safe_zone[1][1])
-            newpose[2] = np.clip(newpose[2], self._safe_zone[0][2], self._safe_zone[1][2])
+            newpose = [pos[0], pos[1] + self.xarm_socket.dx, pos[2] + self.xarm_socket.dy]
+            range = np.linalg.norm(newpose)
+            if range < self._min_range:
+                newpose = newpose / np.linalg.norm(newpose) * self._min_range
+            elif range > self._max_range:
+                newpose = newpose / np.linalg.norm(newpose) * self._max_range
+
+            newpose = [newpose[0], newpose[1], max(newpose[2], self._min_height)]
+
+            updated_quaternion = self._get_new_target_orientation(newpose)
+            
             print("pose", pos, "->", newpose, end="")
-            cube.set_world_pose(np.array(newpose))
+            cube.set_world_pose(np.array(newpose), updated_quaternion)
             print("set.")
             self.xarm_socket.dx = None
             self.xarm_socket.dy = None
 
-        # if (self._rxconn):
-        #     try:
-        #         data = self._rxconn.recv(1024, 0x40) # non-blocking receive
-        #         message = data.decode()
-        #         print("received:", type(message), message)
-        #         x, y, z, dx, dy = ast.literal_eval(message)
-        #         print("received:", x, y, z, dx, dy)
-        #         world = self.get_world()
-        #         cube = world.get_object("target")
-        #         pos, _ = cube.get_world_pose()
-        #         newpose = [ pos[0], pos[1]+dy, pos[2]+dx]
-        #         newpose[1] = np.clip(newpose[1], self._safe_zone[0][1], self._safe_zone[1][1])
-        #         newpose[2] = np.clip(newpose[2], self._safe_zone[0][2], self._safe_zone[1][2])
-        #         print("newpose:", newpose)
-        #         cube.set_world_pose(np.array(newpose))
-        #     except:
-        #         # didn't receive anything
-                
-        #         pass
+            self._last_face_seen_time = current_time
+        elif ( \
+                self._xarm_task.task_achieved or \
+                current_time > self._last_rand_target_time + self._last_rand_target_timeout \
+            ) and current_time > self._last_face_seen_time + self._last_face_seen_timeout:
+                # set new location
+                world = self.get_world()
+                cube = world.scene.get_object("target")
+                randpos = [
+                    np.random.uniform(-1, 1), 
+                    np.random.uniform(-1, 1),
+                    np.random.uniform(0, 1)
+                ]
+                range = np.linalg.norm(randpos)
+                if range < self._min_range:
+                    randpos = randpos / np.linalg.norm(randpos) * self._min_range
+                elif range > self._max_range:
+                    randpos = randpos / np.linalg.norm(randpos) * self._max_range
 
+                randpos = [randpos[0], randpos[1], max(randpos[2], self._min_height)]
+
+                updated_quaternion = self._get_new_target_orientation(randpos)
+
+                print("Setting new target pos:"+str(randpos))
+                cube.set_world_pose(np.array(randpos), updated_quaternion)
+
+                self._last_rand_target_time = time.time()
         return
 
     def _on_add_obstacle_event(self):
@@ -157,3 +180,24 @@ class XArmSample(BaseSample):
         data_logger.save(log_path=log_path)
         data_logger.reset()
         return
+    
+    def _get_new_target_orientation(self, position):
+        direction_vector = np.array([0, 0, 0]) - position
+        normalized_direction = direction_vector / np.linalg.norm(direction_vector)
+
+        rotation_axis = np.cross(np.array([0, 0, -1]), normalized_direction)
+        rotation_axis /= np.linalg.norm(rotation_axis)
+
+        rotation_angle = np.arccos(np.dot(np.array([0, 0, -1]), normalized_direction))
+        half_angle = 0.5 * rotation_angle
+
+        sin_half_angle = np.sin(half_angle)
+        cos_half_angle = np.cos(half_angle)
+
+        quaternion = np.array([
+            cos_half_angle,
+            sin_half_angle * rotation_axis[0],
+            sin_half_angle * rotation_axis[1],
+            sin_half_angle * rotation_axis[2]
+        ])
+        return quaternion
